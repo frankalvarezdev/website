ea90b9900ae91af4896848c9e6a2b91b15138861
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-13 21:12:17 -0500

    fuente nueva

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,9 @@ class MyDocument extends Document {
         return (
             <Html lang='es'>
                 <Head>
-                    <link rel='stylesheet' href='https://stc.sandmy.com/css/icons/brands.css' />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />

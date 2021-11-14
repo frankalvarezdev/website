e764e25dc7626f7714368cb63dc8e531f09d85bd
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-13 22:02:14 -0500

    solucion de bug de responsive

--- components/project/Project.js
+++ components/project/Project.js
@@ -4,24 +4,26 @@ import Icon from "components/utils/Icon";
 const Project = ({ name, url, repository, image }) => {
 
     return (
-        <div className='project'>
-            <div className='content'>
-                <h3>
-                    {name}
-                </h3>
+        <div>
+            <div className='project'>
+                <div className='content'>
+                    <h3>
+                        {name}
+                    </h3>
 
-                <a href={url} target='_blank' rel='noreferrer'>
-                    <ImageRectangle src={image} />
-                </a>
-
-
-                <div className='gap:sm project-links'>
-                    <a href={repository} target='_blank' rel='noreferrer' title='repositorio'>
-                        <i className='brand-github' />
+                    <a href={url} target='_blank' rel='noreferrer'>
+                        <ImageRectangle src={image} />
                     </a>
-                    <a href={url} target='_blank' rel='noreferrer' title='Enlace'>
-                        <Icon icon='open_in_new' />
-                    </a>
+
+
+                    <div className='gap:sm project-links'>
+                        <a href={repository} target='_blank' rel='noreferrer' title='repositorio'>
+                            <i className='brand-github' />
+                        </a>
+                        <a href={url} target='_blank' rel='noreferrer' title='Enlace'>
+                            <Icon icon='open_in_new' />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>

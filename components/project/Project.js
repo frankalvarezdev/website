10ce6797ee0670d1004e3d31a2c9f52d6284bc67
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-13 21:21:48 -0500

    project styles

--- components/project/Project.js
+++ components/project/Project.js
@@ -5,19 +5,21 @@ const Project = ({ name, url, repository, image }) => {
 
     return (
         <div className='project'>
-            <h3>
-                {name}
-            </h3>
+            <div className='content'>
+                <h3>
+                    {name}
+                </h3>
 
-            <ImageRectangle src={image} />            
+                <ImageRectangle src={image} />
 
-            <div className='gap:sm project-links'>
-                <a href={repository} target='_blank' rel='noreferrer' title='repositorio'>
-                    <i className='brand-github' />
-                </a>
-                <a href={url} target='_blank' rel='noreferrer' title='Enlace'>
-                    <Icon icon='open_in_new' />
-                </a>
+                <div className='gap:sm project-links'>
+                    <a href={repository} target='_blank' rel='noreferrer' title='repositorio'>
+                        <i className='brand-github' />
+                    </a>
+                    <a href={url} target='_blank' rel='noreferrer' title='Enlace'>
+                        <Icon icon='open_in_new' />
+                    </a>
+                </div>
             </div>
         </div>
     )

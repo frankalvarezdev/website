77a17ebc9cf4db4e32bc2c9807e4c59a5456cef5
Frank Alvarez <64846247+frankalvarezdev@users.noreply.github.com>
2023-01-08 19:56:39 -0500

    Responsive Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,14 +8,18 @@ const Banner = () => {
                 <Grid gridTemplateColumns={{
                     base: '1fr',
                     md: '1fr 1fr'
-                }}>
-                    <Flex alignItems='center'>
-                        <Box>
-                            <Heading fontWeight='black' as='h1' fontSize='6xl' lineHeight='1em'>
+                }} gap={8}>
+                    <Flex alignItems='center' w='full'>
+                        <Box textAlign={{ base: 'center', md: 'left' }} w='full'>
+                            <Heading fontWeight='black' as='h1' lineHeight='1em'
+                                fontSize={{ base: '5xl', md: '6xl' }}
+                            >
                                 Frank Alvarez
                             </Heading>
 
-                            <Text fontSize='3xl' mb={2} fontWeight={300}>
+                            <Text mb={2} fontWeight={300}
+                                fontSize={{ base: '2xl', md: '3xl' }}
+                            >
                                 Desarrollador web
                             </Text>
 
@@ -27,17 +31,18 @@ const Banner = () => {
                         </Box>
                     </Flex>
 
-                    <Flex justifyContent='flex-end' alignItems='center'>
+                    <Flex justifyContent={{ base: 'center', md: 'flex-end' }} alignItems='center'>
                         <Image
                             alt='Foto de perfil'
                             src='https://cdn.frankalvarez.dev/profile.jpg'
                             borderRadius='57% 43% 58% 42% / 44% 47% 53% 56%'
                             boxSize='320px'
+                            maxW='full'
                         />
                     </Flex>
                 </Grid>
             </Container>
-        </Box>
+        </Box >
     )
 }
 
